0a5c6654eafcb2201fd055e892a9f18752206d6a
Quildreen Motta <quildreen@gmail.com>
2011-01-16 22:25:27 -0200

    Moves shared utilities to utils.py

---
+++ utils.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2010 Quildreen Motta
+# Released under the MIT licence
+
+"""Utilities for rhythmbox dbus scripts.
+"""
+
+import urllib
+
+from subprocess import call
+
+
+def sanitize(uri):
+    """Sanitizes the given URI to a local filename
+    """
+    return urllib.unquote(uri).replace("file://", "")
+    
+
+def notify(summary, text, icon=None):
+    """Notifies the user through libnotify
+    """
+    cmd = ["notify-send", summary, text]
+    if icon is not None:
+        cmd.extend(["-i", icon])
+
+    call(cmd)
